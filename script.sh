8c6328a41dc51310b4967838895beff298166031
Shreyans Jain <31065577+shreyanse081@users.noreply.github.com>
2019-06-05 21:02:14 +0530

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,3 @@
+pip install --upgrade pip
+apt update && apt install -y libsm6 libxext6 libxrender-dev git unzip 
+pip install keras matplotlib image pandas opencv-python opencv-contrib-python imgaug h5py
